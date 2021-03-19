6797b7d85fdf00cdaae8e249fbef09e6c185e4f0
Martin Siönäs <martin.sionas@gmail.com>
2021-03-19 13:17:19 +0100

    Added test for StringCalculator.add with different separators. Works as intended

--- src/test/java/StringCalculator/StringCalcTest.java
+++ src/test/java/StringCalculator/StringCalcTest.java
@@ -31,4 +31,9 @@ public class StringCalcTest {
         assertEquals(3, calc.add("1\n2"));
     }
 
+    @Test
+    void addInputWithDifferentSeparation() {
+        assertEquals(7, calc.add("2, 4\n1"));
+    }
+
 }
