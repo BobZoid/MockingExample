7d6c61fbe48984d2a7db583891bd55d2ae9636c2
Martin Siönäs <martin.sionas@gmail.com>
2021-03-23 14:56:55 +0100

    Added two more tests for payEmployees() in EmployeeManager class. Method both catches runtimeexceptions in Bankservice.pay() method and can handle an empty list of employees.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.stubbing.OngoingStubbing;
 
 import java.util.List;
 
@@ -24,5 +25,24 @@ class EmployeeManagerTest {
         when(empRepo.findAll()).thenReturn(List.of(new Employee("a", 1), new Employee("b", 2), new Employee("c", 3)));
         EmployeeManager emp = new EmployeeManager(empRepo, bankService);
         assertEquals(3, emp.payEmployees());
+    }
+
+    @Test
+    void payEmpCatchesException() {
+        EmployeeRepository empRepo = mock(EmployeeRepository.class);
+        when(empRepo.findAll()).thenReturn(List.of(new Employee("a", 1), new Employee("b", 2), new Employee("c", 3)));
+        BankService bankService = mock(BankService.class);
+        doThrow(new RuntimeException()).when(bankService).pay("a", 1);
+        EmployeeManager emp = new EmployeeManager(empRepo, bankService);
+        assertEquals(2, emp.payEmployees());
+    }
+
+    @Test
+    void payEmpWithEmptyList() {
+        EmployeeRepository empRepo = mock(EmployeeRepository.class);
+        when(empRepo.findAll()).thenReturn(List.of());
+        BankService bankService = mock(BankService.class);
+        EmployeeManager emp = new EmployeeManager(empRepo, bankService);
+        assertEquals(0, emp.payEmployees());
     }
 }
