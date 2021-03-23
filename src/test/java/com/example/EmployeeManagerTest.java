c0e4694a45e24790a7266146dd68b8c917b129c3
Martin Siönäs <martin.sionas@gmail.com>
2021-03-23 14:12:57 +0100

    Added first test for payEmployees() method in EmployeeManager using Mockito.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,15 +1,28 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
 
-    EmployeeManager emp = new EmployeeManager(new EmployeeRepositoryStub(), new BankServiceDouble());
+
 
     @Test
     void payEmpTest() {
+        EmployeeManager emp = new EmployeeManager(new EmployeeRepositoryStub(), new BankServiceDouble());
         assertEquals(3, emp.payEmployees());
+    }
 
+    @Test
+    void payEmpTestWithMockito() {
+        EmployeeRepository empRepo = mock(EmployeeRepository.class);
+        BankService bankService = mock(BankService.class);
+        when(empRepo.findAll()).thenReturn(List.of(new Employee("a", 1), new Employee("b", 2), new Employee("c", 3)));
+        EmployeeManager emp = new EmployeeManager(empRepo, bankService);
+        assertEquals(3, emp.payEmployees());
     }
 }
