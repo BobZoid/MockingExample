9802d44fe345d636972459f1374b01e2531b1b20
Martin Siönäs <martin.sionas@gmail.com>
2021-03-24 12:54:59 +0100

    Second and third test of RepositoryOfEmployees proceeds without problems.

--- src/test/java/LetsManageThisEmployee/RepositoryOfEmployeesTest.java
+++ src/test/java/LetsManageThisEmployee/RepositoryOfEmployeesTest.java
@@ -1,15 +1,32 @@
 package LetsManageThisEmployee;
 
+import com.example.Employee;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RepositoryOfEmployeesTest {
+    RepositoryOfEmployees empRepo = new RepositoryOfEmployees();
+
 
     @Test
     public void findAllOnEmptyReturnsEmptyList() {
-        RepositoryOfEmployees empRepo = new RepositoryOfEmployees();
         var empList = empRepo.findAll();
         assertEquals(0, empList.size());
+    }
+
+    @Test
+    public void findAllAfterSaveReturnsListWithEmployees() {
+        empRepo.save(new Employee("a", 1));
+        empRepo.save(new Employee("b", 2));
+        assertEquals(2, empRepo.findAll().size());
+    }
+
+    @Test
+    public void afterSaveFindAllReturnsCorrectContent() {
+        empRepo.save(new Employee("c", 3));
+        var empList = empRepo.findAll();
+        assertEquals("c", empList.get(0).getId());
+        assertEquals(3, empList.get(0).getSalary());
     }
 }
