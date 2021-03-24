ac9d01109817fb66915758706e77e70d3031d730
Martin Siönäs <martin.sionas@gmail.com>
2021-03-24 14:59:47 +0100

    Added test to IT class to make sure payEmployees catches exception.

--- src/test/java/LetsManageThisEmployee/IT.java
+++ src/test/java/LetsManageThisEmployee/IT.java
@@ -8,16 +8,24 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 public class IT {
+    BankService bankService = mock(BankService.class);
     EmployeeManager manager = new EmployeeManager(new RepositoryOfEmployees(List.of(
             new Employee("a", 1),
             new Employee("b", 2),
-            new Employee("c", 3))), mock(BankService.class));
+            new Employee("c", 3))), bankService);
 
     @Test
     public void payEmployees() {
         assertEquals(3, manager.payEmployees());
+    }
+
+    @Test
+    public void payEmployeesCatchesException() {
+        doThrow(new RuntimeException()).when(bankService).pay("a", 1);
+        assertEquals(2, manager.payEmployees());
     }
 }
